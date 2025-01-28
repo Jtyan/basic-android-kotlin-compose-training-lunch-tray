1d4507287fa6da3c344e22cc0d7891e5114a8761
Joeltt <joelttm15@gmail.com>
2025-01-28 17:30:54 +0000

    feat: implement an app bar composable

--- app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
+++ app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
@@ -15,20 +15,63 @@
  */
 package com.example.lunchtray
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.lunchtray.ui.OrderViewModel
 import androidx.navigation.compose.rememberNavController
 import com.example.lunchtray.model.LunchTrayAppScreenEnum
+import com.example.lunchtray.ui.StartOrderScreen
+import com.example.lunchtray.ui.theme.LunchTrayTheme
 
 // TODO: AppBar
-
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LunchTrayAppBar(
+    currentScreen: LunchTrayAppScreenEnum,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(stringResource(R.string.app_name)) },
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -42,15 +85,35 @@ fun LunchTrayApp(
     // initialize the backstack entry and name of the current screen
     val backStackEntry by navController.currentBackStackEntryAsState()
     // The name of the current screen should be either the name of the initial screen or the name of the destination screen if it currently exists.
-    val currentScreen = LunchTrayAppScreenEnum.valueOf(backStackEntry?.destination?.route ?: LunchTrayAppScreenEnum.START.name)
+    val currentScreen = LunchTrayAppScreenEnum.valueOf(
+        backStackEntry?.destination?.route ?: LunchTrayAppScreenEnum.START.name
+    )
 
     Scaffold(
         topBar = {
             // TODO: AppBar
+            LunchTrayAppBar(
+                canNavigateBack = navController.previousBackStackEntry != null,
+                navigateUp = { navController.navigateUp() },
+                currentScreen = currentScreen
+            )
         }
     ) { innerPadding ->
         val uiState by viewModel.uiState.collectAsState()
 
         // TODO: Navigation host
+
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun LunchTrayAppPreview() {
+    Surface(
+        modifier = Modifier.statusBarsPadding()
+    ) {
+        LunchTrayTheme {
+            LunchTrayApp()
+        }
     }
 }
