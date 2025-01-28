ee27d49f5cbbce58b91a60671b7c33d12639e862
Joeltt <joelttm15@gmail.com>
2025-01-28 15:42:26 +0000

    feat: implement screen enum class

---
+++ app/src/main/java/com/example/lunchtray/model/ScreenEnum.kt
@@ -0,0 +1,12 @@
+package com.example.lunchtray.model
+
+import androidx.annotation.StringRes
+import com.example.lunchtray.R
+
+enum class ScreenEnum(@StringRes val title: Int) {
+    START(title = R.string.start_order),
+    ENTREE_MENU(title = R.string.choose_entree),
+    SIDE_DISH_MENU(title = R.string.choose_side_dish),
+    ACCOMPANIMENT_MENU(title = R.string.choose_accompaniment),
+    CHECKOUT(title = R.string.order_checkout)
+}
